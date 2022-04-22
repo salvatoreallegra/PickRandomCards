446279e1b263fbc398abdec0e6d64620f779e5d1
Salvatore Allegra <salallegra@gmail.com>
2022-04-22 16:10:09 -0700

    Add method stubs for generating random values and random suits

--- CardPicker.cs
+++ CardPicker.cs
@@ -10,7 +10,23 @@ namespace PickRandomCards
     {
         public static string[] pickSomeCards(int numOfCards)
         {
-
+            string[] pickedCards = new string[numOfCards];
+            for (int i = 0; i < numOfCards; i++)
+            {
+                pickedCards[i] = randomValue() + " of " + randomSuit();
+            }
+            return pickedCards;
         }
+
+        public static string randomValue()
+        {
+            return "";
+        }
+
+        public static string randomSuit()
+        {
+            return "";
+        }
+        
     }
 }
